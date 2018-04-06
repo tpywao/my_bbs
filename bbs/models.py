f88b621a497914f61063b43bd0355610762cebc6
tpywao <tpywao@gmail.com>
2018-04-07 03:12:17 +0900

    モデルを作る

--- bbs/models.py
+++ bbs/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Thread(models.Model):
+    title = models.CharField(max_length=255)
+    contributor_name = models.CharField(max_length=255)
+    created_time = models.DateTimeField(auto_now_add=True)
+
+
+class Response(models.Model):
+    thread = models.ForeignKey(Thread)
+    contributor_name = models.CharField(max_length=255)
+    content = models.TextField()
+    created_time = models.DateTimeField(auto_now_add=True)
