4c2b5982d70af88538a7074ac38940edf6ae0a06
tpywao <tpywao@gmail.com>
2018-04-07 03:45:55 +0900

    スレッド一覧画面を作る

--- my_bbs/urls.py
+++ my_bbs/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from bbs.views import ThreadList
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^bbs/$', ThreadList.as_view()),
 ]
